0990095490a8166da36a4fc0c42a445d7f9e8428
장현웅 <jhw811@gmail.com>
2023-03-03 01:31:38 +0900

    로고 적용

--- src/templates/Slider.tsx
+++ src/templates/Slider.tsx
@@ -1,16 +1,18 @@
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import styled from 'styled-components';
-import AVALANCHE from '../assets/images/logos/AVALANCHE.png';
-import BCi from '../assets/images/logos/BCi.png';
-import GLITCH from '../assets/images/logos/GLITCH.svg';
-import Lifefourcuts from '../assets/images/logos/Lifefourcuts.svg';
-import METABEAT from '../assets/images/logos/METABEAT.png';
-import oxsa from '../assets/images/logos/oxsa.svg';
-import smart from '../assets/images/logos/smart.png';
-import TSBLOC from '../assets/images/logos/TSBLOC.svg';
+import biotherm from '../assets/images/logos/biotherm.png';
+import dyson from '../assets/images/logos/dyson.png';
+import gillette from '../assets/images/logos/gillette.png';
+import hyundai from '../assets/images/logos/hyundai.png';
+import samsung from '../assets/images/logos/samsung.png';
+import sbs from '../assets/images/logos/sbs.png';
+import sk from '../assets/images/logos/sk.png';
+import universal from '../assets/images/logos/universal.png';
+import warner from '../assets/images/logos/warner.png';
+import wing from '../assets/images/logos/wing.png';
 
-const IMAGE_LIST = [AVALANCHE, BCi, GLITCH, Lifefourcuts, METABEAT, oxsa, smart, TSBLOC];
+const IMAGE_LIST = [samsung, sk, gillette, wing, biotherm, dyson, warner, hyundai, universal, sbs];
 
 const BannerSlider = () => {
   const settings = {
@@ -40,16 +42,17 @@ const SlickImageContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  height: 20px;
-  margin: 50px;
+  height: 60px;
+  margin: 10px;
   position: relative;
   @media screen and (max-width: 768px) {
-    height: 12px;
-    margin: 25px;
+    height: 30px;
+    margin: 20px;
   }
 `;
 
 const SlickImage = styled.img`
   height: 100%;
+  /* transform: scale(0.3); */
 `;
 export default BannerSlider;
